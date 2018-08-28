d73f5dc5250ce4c61678430aeee15f0190cc8159
Alaan Rahim <alaanrahim@Alaans-MacBook-Pro.local>
2018-08-28 16:05:19 +0100

    Initial Mantle project set-up with BPrivacy encryption/decryption methods exposed

---
+++ test/mantle.spec.js
@@ -0,0 +1,28 @@
+import Mantle from '../src/mantle'
+
+describe('Mantle', () => {
+  test('throws an error if no configuration is provided', () => {
+    const error = 'No configuration provided: cannot initialize Mantle'
+    expect(() => {
+      new Mantle(null)
+    }).toThrow(error)
+  })
+
+  describe('Web3 integration', () => {
+    test('exposes the web3 API to the instance after successful connection', () => {
+      const mantle = new Mantle()
+      expect(mantle.web3).toBeUndefined()
+      mantle.connect()
+      expect(mantle.web3).toBeTruthy()
+    })
+
+    test('exposes the Web3 module as a class property', () => {
+      expect(Mantle.Web3.version).toBe('1.0.0-beta.35')
+    })
+
+    test('exposes Web3 module as an instance property', () => {
+      const mantle = new Mantle()
+      expect(mantle.Web3.version).toBe('1.0.0-beta.35')
+    })
+  })
+})
