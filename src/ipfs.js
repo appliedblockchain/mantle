d45e7c0b5c77e5bc0dd5c7c4ddda7c82d539f824
Alaan Rahim <alaanrahim@Alaans-MacBook-Pro.local>
2018-09-06 09:35:05 +0100

    remove attachToObject

--- src/ipfs.js
+++ src/ipfs.js
@@ -42,43 +42,6 @@ class IPFS {
     await this.pin.add(hash)
     return hash
   }
-
-  /**
-   * @param  {string} hash
-   * @return {buffer}
-   */
-  async retrieve(hash) {
-    const retrieved = await this.files.cat(hash)
-    return retrieved
-  }
-
-  /**
-   * Removes data from IPFS by unpinning the hash and then running
-   * the IPFS garbage collector
-   * @param  {string} hash
-   * @return {void}
-   */
-  async remove(hash) {
-    await this.pin.rm(hash)
-    this.repo.gc()
-  }
-
-  /**
-   * Stores data on IPFS and pins the hash to the pinset
-   * @param  {buffer} data
-   * @return {string}
-   */
-  async store(data) {
-    if (!Buffer.isBuffer(data)) {
-      data = Buffer.from(data)
-    }
-
-    const [ storedData ] = await this.add(data)
-    const { hash } = storedData
-
-    await this.pin.add(hash)
-    return hash
-  }
 }
 
 module.exports = IPFS
