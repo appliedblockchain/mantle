f9571921adba94a1f65f7ed2a9962faab52a9ca4
Alaan Rahim <alaanrahim@Alaans-MacBook-Pro.local>
2018-09-13 14:10:53 +0100

    change signing and hashing method - signing should be passed a string, not a hash

--- test/mantle.spec.js
+++ test/mantle.spec.js
@@ -64,22 +64,20 @@ describe('Mantle', () => {
       }).toThrow()
     })
 
-    test('throws an error when providing an invalid hash', () => {
-      const hash = '@invalid_hash'
+    test('throws an error when providing an invalid message', () => {
       const mantle = new Mantle()
 
       mantle.loadMnemonic()
       expect(() => {
-        Mantle.sign(hash, mantle.privateKey)
+        Mantle.sign(null, mantle.privateKey)
       }).toThrow()
     })
 
     test('generates a message signature via sign()', () => {
-      const hash = Mantle.generateHash(data)
       const mantle = new Mantle()
 
       mantle.loadMnemonic()
-      const signature = Mantle.sign(hash, mantle.privateKey)
+      const signature = Mantle.sign(data, mantle.privateKey)
 
       expect(signature).toBeTruthy()
       expect(signature.startsWith('0x')).toBe(true)
@@ -88,11 +86,10 @@ describe('Mantle', () => {
 
   describe('Recovery', () => {
     test('throws an error when providing an invalid hash', () => {
-      const hash = Mantle.generateHash(data)
       const mantle = new Mantle()
 
       mantle.loadMnemonic()
-      const signature = Mantle.sign(hash, mantle.privateKey)
+      const signature = Mantle.sign(data, mantle.privateKey)
 
       const invalidHash = '@invalid_hash'
 
@@ -106,7 +103,7 @@ describe('Mantle', () => {
       const mantle = new Mantle()
 
       mantle.loadMnemonic()
-      Mantle.sign(hash, mantle.privateKey)
+      Mantle.sign(data, mantle.privateKey)
 
       const invalidSignature = '@invalid_signature'
 
@@ -120,7 +117,7 @@ describe('Mantle', () => {
       const mantle = new Mantle()
 
       mantle.loadMnemonic()
-      const signature = Mantle.sign(hash, mantle.privateKey)
+      const signature = Mantle.sign(data, mantle.privateKey)
       const publicKey = Mantle.recover(hash, signature)
 
       expect(publicKey).toEqual('0x' + mantle.publicKey.toString('hex'))
