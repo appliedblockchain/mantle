34a06b77afc94a320ef2b7e9c24e968b61708bbf
Dean Richer <dpbricher@gmail.com>
2018-11-05 17:26:53 +0000

    Removed unused API middleware

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,9 +1,7 @@
-const assignToKoaContext = require('./assignToKoaContext')
 const errorHandler = require('./errorHandler')
 const notFoundHandler = require('./notFoundHandler')
 
 module.exports = {
-  assignToKoaContext,
   errorHandler,
   notFoundHandler
 }
