c718edee994537d7eaa94f659362d9a315b55747
Dean Richer <dpbricher@gmail.com>
2018-11-06 17:59:08 +0000

    Added usage docs to API

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
+/** @module mantle/api */
+
+/**
+ * @func module:mantle/api#createServer
+ * @see module:mantle/api/server#createServer
+ */
 const createServer = require('./server')
+
 const router = require('./router')
 
 module.exports = {
   createServer,
+  /**
+   * @const module:mantle/api#ipfs
+   * @see module:mantle/api/router.ipfs
+   */
+  /**
+   * @const module:mantle/api#transactions
+   * @see module:mantle/api/router.transactions
+   */
   ...router
 }
