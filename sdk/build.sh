895970858d787439f475026f69ae8d8069f4cbc4
Rémi Bouchar <remi.bouchar@gmail.com>
2018-12-03 15:48:20 +0000

    bump version, fix bitcore-lib peerdep

--- sdk/build.sh
+++ sdk/build.sh
@@ -4,6 +4,7 @@ set -ex
 
 node uglify.js
 cp package.json ./dist
+cp package-lock.json ./dist
 cd ./dist
 sed -i '-backup' 's/"private": true,/"private": false,/g' package.json
 sed -i '-backup' 's/"name": "@appliedblockchain\/mantle",/"name": "@appliedblockchain\/mantle-core",/g' package.json
