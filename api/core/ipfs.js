b8b4910d81d68a462ab5179853a0fb66b4d69144
Alaan Rahim <alaan@appliedblockchain.com>
2019-01-07 12:03:06 +0000

    Extract business logic to `core`

---
+++ api/core/ipfs.js
@@ -0,0 +1,19 @@
+const ipfsAPI = require('ipfs-api')
+const defaultIpfs = ipfsAPI(process.env.IPFS_HOST || '127.0.0.1')
+
+const add = async (data, ipfs = defaultIpfs) => {
+  const [ storedData ] = await ipfs.add(Buffer.from(data))
+  const { hash } = storedData
+  await ipfs.pin.add(hash)
+  return hash
+}
+
+const remove = async (hash, ipfs = defaultIpfs) => {
+  await ipfs.pin.rm(hash)
+  await ipfs.repo.gc()
+}
+
+module.exports = {
+  add,
+  remove
+}
--- api/router/routes/ipfs/handlers.js
+++ api/router/routes/ipfs/handlers.js
@@ -1,3 +1,5 @@
+const ipfs = require('../../../core/ipfs')
+
 const pin = async ctx => {
   const { hash } = ctx.request.params
   const retrievedPin = await ctx.ipfs.pin.ls(hash)
@@ -12,17 +14,14 @@ const get = async ctx => {
 
 const store = async ctx => {
   const { data } = ctx.request.body
-  const [ storedData ] = await ctx.ipfs.add(Buffer.from(data))
-  const { hash } = storedData
-  await ctx.ipfs.pin.add(hash)
+  const hash = await ipfs.add(data, ctx.ipfs)
   ctx.body = hash
 }
 
 const remove = async ctx => {
   // TODO: Authenticate deletion requests
   const { hash } = ctx.request.params
-  await ctx.ipfs.pin.rm(hash)
-  await ctx.ipfs.repo.gc()
+  await ipfs.remove(hash, ctx.ipfs)
   ctx.status = 204
 }
 
